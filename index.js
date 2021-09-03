443bc1467f5067758aa6dfc95d227b527bd71a69
he141142 <pxz41brxc@gmail.com>
2021-09-04 00:49:13 +0700

    committ

---
+++ index.js
@@ -0,0 +1,10 @@
+// const request = new Request('https://www.mozilla.org/favicon.ico');
+
+// const url = request.url;
+// const method = request.method;
+// const credentials = request.credentials;
+const fs =  require('./2-utils')
+
+fs.func()
+// console.log(__dirname)
+
